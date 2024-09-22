4d57621abc81d0dc63ccd598e9d5112dc033e0de
Jorge de la Luna Mesa <151177853+Jorgedelaluna@users.noreply.github.com>
2024-09-22 12:26:25 +0200

    update

--- T1/Fichero/src/Entrada.java
+++ T1/Fichero/src/Entrada.java
@@ -1,20 +1,16 @@
 import java.io.File;
+import java.net.URI;
 
 public class Entrada {
 
     public static void main(String[] args) {
         //FILE -> fichero logico -> fisico
         // fichero logico
-        // File ficheroSinPuntero = new File( pathname:"C:\\Users\\jdela\\GitHub\\ClaseAD-UNIR\\T1\\Fichero\\src\\resources\\directorio\\ejemplo_fichero.txt");
-        File ficheroSinPuntero = new File( pathname:"src/resources/directorio/ejemplo_fichero.txt");
+        // File ficheroSinPuntero = new File( pathname:"C:\Users\jdela\GitHub\ClaseAD-UNIR\T1\Fichero");
+        File ficheroSinPuntero = new File( pathname:"src/resources/directorio/ejemplo_fichero.md");
         System.out.println(ficheroSinPuntero.getName());
         System.out.println(ficheroSinPuntero.getParent());
         System.out.println(ficheroSinPuntero.length());
-        System.out.println(ficheroSinPuntero.isDirectory());
-        // File [] -> todos los FICHEROS que estan dentro del directorio
-        ficheroSinPuntero.listFiles();
-        // String[] -> todos las rutas de los FICHEROS que estan dentro del directorio
-        ficheroSinPuntero.list();
-
+        //System.out.println(ficheroSinPuntero.isDirectory());
     }
 }
